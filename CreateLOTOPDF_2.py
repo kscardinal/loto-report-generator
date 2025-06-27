2969aecaa4e846567682d623538041dbebc46e5d
kscardinal <kschang@cardinalhs.net>
2025-06-27 10:38:22 -0400

    working on reformatting the code

--- CreateLOTOPDF_2.py
+++ CreateLOTOPDF_2.py
@@ -1,5 +1,5 @@
 # DEPENDECIES
-# pillow, reportlab, ice cream
+# pillow, reportlab, icecream
 
 # Import Functions
 from PIL import Image
@@ -37,41 +37,41 @@ def resize_image(filename, max_height=None, max_width=None):
     return new_height, new_width
 
 # Error message
-def printError(errorMessage):
+def print_error(errorMessage):
     redColor = "\033[91m"
     whiteColor = "\033[0m"
     print(f"{redColor}{errorMessage}{whiteColor}")
 
 # Success message
-def printSuccess(successMessage):
+def print_success(successMessage):
     greenColor = "\033[92m"
     whiteColor = "\033[0m"
     print(f"{greenColor}{successMessage}{whiteColor}")
 
 # Load json/data
-def loadData(fileName):
+def load_data(fileName):
     try:
         with open(fileName, "r") as file:
             return json.load(file)
     except  Exception as e:
-        printError(f"Could not load json file: {e}")
+        print_error(f"Could not load json file: {e}")
 
 # Get data from json json
-def getData(data, entryName):
+def get_data(data, entryName):
     if entryName in data:
         return data[entryName]
     
 # Get number of pages
-def pageCount(pdf_path):
+def page_count(pdf_path):
     try:
         reader = PdfReader(pdf_path)
         return len(reader.pages)
     except Exception as e:
-        printError(f"Error reading PDF file: {e}")
+        print_error(f"Error reading PDF file: {e}")
         return 0
 
 # Processes text for placement on PDF
-def processText(input_text, line_length, max_lines, return_lines=False):
+def process_text(input_text, line_length, max_lines, return_lines=False):
     words = input_text.split()
     lines = []
     current_line = []
@@ -91,47 +91,54 @@ def processText(input_text, line_length, max_lines, return_lines=False):
     return lines if return_lines else len(lines)
 
 # Splits text into lines
-def splitText(input_text, line_length, max_lines):
-    return processText(input_text, line_length, max_lines, return_lines=True)
+def split_text(input_text, line_length, max_lines):
+    return process_text(input_text, line_length, max_lines, return_lines=True)
 
 # Returns number of lines
-def numLines(input_text, line_length, max_lines):
-    return processText(input_text, line_length, max_lines, return_lines=False)
+def num_lines(input_text, line_length, max_lines):
+    return process_text(input_text, line_length, max_lines, return_lines=False)
 
 # Check to see if variable exists
-def checkExists(fields, field_name):
+def check_exists(fields, field_name):
     if fields.get(field_name):
         return True
     else:
         return False
     
 # Checks the length of a string and truncates it
-def checkLength(text, max_length, add_ellipsis=False):
+def check_length(text, max_length, add_ellipsis=False):
     if len(text) > max_length:
         return text[:max_length] + " ..." if add_ellipsis else text[:max_length]
     return text
 
-# GLOBAL VARIABLES
-# Page Setup
-pageHeight = 792                                                # 11in (1pt = 1/72 inch)
-pageWidth = 612                                                  # 8.5in
-pageSize = pageWidth, pageHeight
-pageWidth_Middle = pageWidth / 2
-pageHeight_Middle = pageHeight / 2
-pageMargin = 36                                                 # 0.50in
-page_LeftMargin = pageMargin
-page_RightMargin = pageWidth - pageMargin
-page_MarginWidth = page_RightMargin - page_LeftMargin
+
+# Global variables for page setup and defaults
+
+# Page dimensions (in points, 1 pt = 1/72 inch)
+PAGE_HEIGHT = 792                                                # 11 inches
+PAGE_WIDTH = 612                                                  # 8.5 inches
+PAGE_SIZE = (PAGE_WIDTH, PAGE_HEIGHT)
+PAGE_MARGIN = 36                                                 # 0.50in
+PAGE_LEFT_MARGIN = PAGE_MARGIN
+PAGE_RIGHT_MARGIN = PAGE_WIDTH - PAGE_MARGIN
+USABLE_WIDTH = PAGE_RIGHT_MARGIN - PAGE_LEFT_MARGIN
+PAGE_WIDTH_MIDDLE = PAGE_WIDTH / 2
+PAGE_HEIGHT_MIDDLE = PAGE_HEIGHT / 2
+
+# Default
+DEFAULT_LINE_WIDTH  = 0.25           # This was arbitray but looks like what it was
+DEFAULT_COLOR  = [0, 0, 0]               # Black
+
 
 # Data
-dataFileName = 'data_4.json'
-data = loadData(dataFileName)
+data_file = 'data_4.json'
+data = load_data(data_file)
 
 
 # Creating PDF and setting document title
-fileName = dataFileName[:-5]
-pdf = canvas.Canvas(fileName + "_WIP_2.pdf", pageSize)
-pdf.setTitle(fileName)
+file_name = data_file[:-5]                                # Gets data file name and strips the '.json' so we have the name of the machine
+pdf = canvas.Canvas(file_name + "_WIP_2.pdf", PAGE_SIZE)          # Makes the pdf with the filename from the line above and adds extension '.pdf'
+pdf.setTitle(file_name)       # Sets document title. Appears only in document properties
 
 
 # Registering Fonts
@@ -140,24 +147,20 @@ pdfmetrics.registerFont(TTFont('Inter', 'Inter_Regular.ttf'))
 pdfmetrics.registerFont(TTFont('Times', 'times.ttf'))
 
 
-# Default
-defaultLineWidth = 0.25
-defaultColor = [0, 0, 0]                                 # Black
-
 # Adds Header to current page
 def addHeader():
     # Header Title
     header_Title_FontSize = 18
     header_Title_Font = 'DM Serif Display'
     header_Title_LineSpacing = 20
-    header_Title_Y = pageHeight - 54             # 0.75in
+    header_Title_Y = PAGE_HEIGHT - 54             # 0.75in
 
     # Header Image
     header_Image_Name = 'CardinalLogo.png'
     header_Image_Width = 144                               # 2in
     header_Image_Height = 72
     header_Image_Height, header_Image_Width = resize_image(header_Image_Name, header_Image_Height, header_Image_Width)
-    header_Image_X = page_LeftMargin
+    header_Image_X = PAGE_LEFT_MARGIN
     header_Image_Y = header_Title_Y - header_Image_Height + header_Title_LineSpacing
 
     # Header Field Options
@@ -178,7 +181,7 @@ def addHeader():
     # Header Description
     header_Field_Description_LineLength = 78
     header_Field_Description_LineLimit = 5
-    header_Field_Description_Height = numLines(data['Description'], header_Field_Description_LineLength, header_Field_Description_LineLimit)
+    header_Field_Description_Height = num_lines(data['Description'], header_Field_Description_LineLength, header_Field_Description_LineLimit)
     
     # Header Procedure Number
     header_Field_ProcedureNumber_LineLength = 16
@@ -186,12 +189,12 @@ def addHeader():
     # Header Facility
     header_Field_Facility_LineLength = 26
     header_Field_Facility_LineLimit = 5
-    header_Field_Facility_Height = numLines(data['Facility'], header_Field_Facility_LineLength, header_Field_Facility_LineLimit)
+    header_Field_Facility_Height = num_lines(data['Facility'], header_Field_Facility_LineLength, header_Field_Facility_LineLimit)
 
     # Header Location
     header_Field_Location_LineLength = 26
     header_Field_Location_LineLimit = 5
-    header_Field_Location_Height = numLines(data['Location'], header_Field_Location_LineLength, header_Field_Location_LineLimit)
+    header_Field_Location_Height = num_lines(data['Location'], header_Field_Location_LineLength, header_Field_Location_LineLimit)
     
     # Header Revision
     header_Field_Revision_LineLength = 6
@@ -210,12 +213,12 @@ def addHeader():
     header_Field_H_Line5 = header_Field_H_Line4 - header_Field_Row3_Offset
 
     # Vertical Lines (1 = left, 6 = Right)
-    header_Field_V_Line1 = page_LeftMargin
-    header_Field_V_Line2 = page_LeftMargin + ((page_MarginWidth - header_Field_Address_Width) / 2) - (header_Field_Revision_Width / 2)
-    header_Field_V_Line3 = page_RightMargin - header_Field_Address_Width - header_Field_Revision_Width
-    header_Field_V_Line4 = page_RightMargin - header_Field_Address_Width
-    header_Field_V_Line5 = page_RightMargin - (header_Field_Address_Width * (2 / 5))
-    header_Field_V_Line6 = page_RightMargin
+    header_Field_V_Line1 = PAGE_LEFT_MARGIN
+    header_Field_V_Line2 = PAGE_LEFT_MARGIN + ((USABLE_WIDTH - header_Field_Address_Width) / 2) - (header_Field_Revision_Width / 2)
+    header_Field_V_Line3 = PAGE_RIGHT_MARGIN - header_Field_Address_Width - header_Field_Revision_Width
+    header_Field_V_Line4 = PAGE_RIGHT_MARGIN - header_Field_Address_Width
+    header_Field_V_Line5 = PAGE_RIGHT_MARGIN - (header_Field_Address_Width * (2 / 5))
+    header_Field_V_Line6 = PAGE_RIGHT_MARGIN
 
     # Text Locations - Rows
     header_Field_Row1_Text = header_Field_H_Line1 - header_Field_Title_FontSize
@@ -258,12 +261,12 @@ def addHeader():
 
     # Creating Header Title
     pdf.setFont(header_Title_Font, header_Title_FontSize)
-    pdf.drawCentredString(pageWidth_Middle, header_Title_Y, "LOCKOUT-TAGOUT")
-    pdf.drawCentredString(pageWidth_Middle, header_Title_Y - header_Title_LineSpacing, "PROCEDURE")
+    pdf.drawCentredString(PAGE_WIDTH_MIDDLE, header_Title_Y, "LOCKOUT-TAGOUT")
+    pdf.drawCentredString(PAGE_WIDTH_MIDDLE, header_Title_Y - header_Title_LineSpacing, "PROCEDURE")
     pdf.drawImage('CardinalLogo.png', header_Image_X, header_Image_Y, header_Image_Width, header_Image_Height )
 
     # Creating Header Field Outlines
-    pdf.setLineWidth(defaultLineWidth)
+    pdf.setLineWidth(DEFAULT_LINE_WIDTH )
 
     # Horizontal Outline Lines
     pdf.line(header_Field_V_Line4, header_Field_H_Line1, header_Field_V_Line6, header_Field_H_Line1)
@@ -294,12 +297,12 @@ def addHeader():
     # Creating Header Field Text
     pdf.setFont(header_Field_Body_Font, header_Field_Body_FontSize)
     for line in range(header_Field_Description_Height):
-        pdf.drawString(headerField_Description_X, header_Field_Row2_Text - (line * header_Field_Body_LineSpacing), splitText(data['Description'], header_Field_Description_LineLength, header_Field_Description_LineLimit)[line])
-    pdf.drawString(headerField_ProcedureNumber_X, header_Field_Row2_Text, checkLength(data['ProcedureNumber'], header_Field_ProcedureNumber_LineLength, False))
+        pdf.drawString(headerField_Description_X, header_Field_Row2_Text - (line * header_Field_Body_LineSpacing), split_text(data['Description'], header_Field_Description_LineLength, header_Field_Description_LineLimit)[line])
+    pdf.drawString(headerField_ProcedureNumber_X, header_Field_Row2_Text, check_length(data['ProcedureNumber'], header_Field_ProcedureNumber_LineLength, False))
     for line in range(header_Field_Facility_Height):
-        pdf.drawString(headerField_Facility_X, header_Field_Row3_Text - (line * header_Field_Body_LineSpacing), splitText(data['Facility'], header_Field_Facility_LineLength, header_Field_Facility_LineLimit)[line])
+        pdf.drawString(headerField_Facility_X, header_Field_Row3_Text - (line * header_Field_Body_LineSpacing), split_text(data['Facility'], header_Field_Facility_LineLength, header_Field_Facility_LineLimit)[line])
     for line in range(header_Field_Location_Height):
-        pdf.drawString(headerField_Location_X, header_Field_Row3_Text - (line * header_Field_Body_LineSpacing), splitText(data['Location'], header_Field_Location_LineLength, header_Field_Location_LineLimit)[line])
+        pdf.drawString(headerField_Location_X, header_Field_Row3_Text - (line * header_Field_Body_LineSpacing), split_text(data['Location'], header_Field_Location_LineLength, header_Field_Location_LineLimit)[line])
     pdf.drawString(headerField_Revision_X, header_Field_Row3_Text, data['Revision'][0:header_Field_Revision_LineLength])
     pdf.setFont("Inter", 9)
     pdf.drawString(headerField_Date_X, header_Field_Row3_Text, data['Date'])
