d8317f8e6168ff38a9a55b83e10eacbe132c4ad4
kscardinal <kschang@cardinalhs.net>
2025-10-28 16:01:14 -0400

    removed typescript stuff for now

--- src/web/static/scripts/input_form.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function addSource(): void;
-export declare function removeLastSource(): void;
-export declare function gatherData(): Record<string, any>;
-export declare function generateJSON(): void;
-export declare function downloadOutput(): void;
-//# sourceMappingURL=input_form.d.ts.map
