4a9454fe4a55b3f4942d85ac0a96f5eed1a33f47
kscardinal <kschang@cardinalhs.net>
2025-11-04 10:58:15 -0500

    working on sources

--- src/web/static/js/update_report.js
+++ src/web/static/js/update_report.js
@@ -4,6 +4,18 @@ backButton.addEventListener("click", async function() {
     console.log("Clicked Back Button!");
 });
 
+// Update Button
+const updateButton = document.getElementById("updateBtn");
+updateButton.addEventListener("click", function() {
+    console.log("Clicked Update Button!")
+});
+
+// Add Source Button
+const addSourceButton = document.getElementById("addSourceBtn");
+addSourceButton.addEventListener("click", function() {
+    console.log("Clicked Add Source Button!");
+});
+
 async function loadData() {
     const response = await fetch(`/get_report_json/${reportName}`);
     if (!response.ok) throw new Error("Report not found");
@@ -14,19 +26,24 @@ async function loadData() {
 
 async function setName() {
     const reportName = document.getElementById("name");
-    
     if (window.reportName) {
         reportName.value = window.reportName;
         reportName.disabled = true;
     } else {
         reportName.disabled = false;
     }
-
 }
 
 async function setData_text(elementID, data) {
     const element = document.getElementById(elementID);
     const element_value = data[elementID];
+    if (!element_value) { return };
+    element.value = element_value;
+}
+
+async function setSourceData_text(elementID, dataID, data) {
+    const element = document.getElementById(elementID);
+    const element_value = data[dataID];
     if (!element_value) { return };
     element.value = element_value;
 }
@@ -67,6 +84,46 @@ async function setData_photo(elementID, data) {
     });
 }
 
+async function setSourceData_photo(elementID, dataID, data) {
+    const input_element = document.getElementById(elementID);
+    const element = document.getElementById(elementID + "_preview");
+    const image_name = data[dataID];
+    if (!image_name) { return }
+
+    const response = await fetch(`/photo_by_name/${image_name}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch image');
+    }
+    const blob = await response.blob();
+    const imageUrl = URL.createObjectURL(blob);
+    element.src = imageUrl;
+    element.style.display = "inline";
+
+    input_element.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const imageUrl = URL.createObjectURL(file);
+            element.src = imageUrl;
+            element.style.display = "inline";
+        } else {
+            element.style.display = "none";
+        }
+    });
+}
+
+async function setData_source(sourceNum, data) {
+    const sourceElement = document.querySelector(`.source#source_${sourceNum + 1}`);
+    const textInputIds = Array.from(sourceElement.querySelectorAll('input[type="text"]')).map(input => input.id);
+    for (const id of textInputIds) {
+        setSourceData_text(id, id.replace(/[^a-zA-Z_]/g, '').replace(/_+$/g, ''), data.sources[sourceNum])
+    }
+    const photoInputIds = Array.from(sourceElement.querySelectorAll('input[type="file"]')).map(input => input.id);
+    for (const id of photoInputIds) {
+        setSourceData_photo(id, id.replace(/[^a-zA-Z_]/g, '').replace(/_+$/g, ''), data.sources[sourceNum])
+    }
+    document.getElementById(`source_${sourceNum + 1}`).style.display = "block";
+}
+
 document.addEventListener('DOMContentLoaded', async function() {
   try {
     setName();
@@ -83,6 +140,9 @@ document.addEventListener('DOMContentLoaded', async function() {
     for (const id of photoInputIds) {
       setData_photo(id, data);
     }
+    await Promise.all(
+        data.sources.map((_, i) => setData_source(i, data))
+    );
   } catch (error) {
     console.error('Error:', error);
   }
