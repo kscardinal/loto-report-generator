4f444787badf3d372715867ec0b1728284a7c66a
Kyle Schang <kyle@schang.net>
2025-11-12 17:00:10 +0000

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 git pull
-docker-compose down
-docker-compose build --no-cache
-docker-compose up -d --remove-orphans
+docker compose down
+docker compose build --no-cache
+docker compose up -d --remove-orphans
 docker image prune -f
