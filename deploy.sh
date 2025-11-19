32f66571bc687881a0184db74bb1f825b016ab87
Kyle Schang <kyle@schang.net>
2025-11-19 22:23:29 +0000

    updated the deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ docker compose down
 docker compose build --no-cache
 docker compose up -d --remove-orphans
 docker image prune -f
-docker compose logs -f
+
+docker compose logs -f 2>&1 | grep -m 1 "Application startup complete."
