81e9c796c7d89b5584991079bf357a9d19a6b8f1
kscardinal <kschang@cardinalhs.net>
2025-11-20 09:15:35 -0500

    updated the deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,12 @@ git pull
 docker compose down
 docker compose build --no-cache
 docker compose up -d --remove-orphans
+
+echo "Application started. Beginning cleanup of unused resources..."
+
 docker image prune -f
+docker builder prune --force
+docker volume prune --force
+docker system prune -f
 
 docker compose logs -f 2>&1 | grep -m 1 "Application startup complete."
